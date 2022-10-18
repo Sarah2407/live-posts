f343c9f268728b7b43d2ac06e2295d8c6c98691f
Sarah Mensah <mansahmensah18@gmail.com>
2022-10-18 12:34:07 +0000

    added authentication using fortify

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/reset-password', function ($token) {
+    return $token;
+})
+    ->middleware(['guest:'.config('fortify.guard')])
+    ->name('password.reset');
